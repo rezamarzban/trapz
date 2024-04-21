8758cebf9a992f68b6561663b2d9047254ca4771
rezamarzban <73051714+rezamarzban@users.noreply.github.com>
2024-04-21 16:30:11 +0330

    Update trapz.py

--- python/trapzV1.1/trapz.py
+++ python/trapzV1.1/trapz.py
@@ -3,10 +3,10 @@ import math
 
 def trapz(expression, a, b, n):
     dx = (b - a) / n
-    total = 0.5 * (eval(expression, {'x': a, 'exp': math.exp, 'sin': math.sin, 'cos': math.cos, 'tan': math.tan, 'cot': lambda x: 1 / math.tan(x), 'ln': math.log, 'log': math.log10, 'sqrt': math.sqrt}) + eval(expression, {'x': b, 'exp': math.exp, 'sin': math.sin, 'cos': math.cos, 'tan': math.tan, 'cot': lambda x: 1 / math.tan(x), 'ln': math.log, 'log': math.log10, 'sqrt': math.sqrt}))
+    total = 0.5 * (eval(expression, {'x': a, 'exp': math.exp, 'sin': math.sin, 'cos': math.cos, 'tan': math.tan, 'cot': lambda x: 1 / math.tan(x), 'log': math.log, 'log10': math.log10, 'sqrt': math.sqrt}) + eval(expression, {'x': b, 'exp': math.exp, 'sin': math.sin, 'cos': math.cos, 'tan': math.tan, 'cot': lambda x: 1 / math.tan(x), 'log': math.log, 'log10': math.log10, 'sqrt': math.sqrt}))
     for i in range(1, n):
         x = a + i * dx
-        total += eval(expression, {'x': x, 'exp': math.exp, 'sin': math.sin, 'cos': math.cos, 'tan': math.tan, 'cot': lambda x: 1 / math.tan(x), 'ln': math.log, 'log': math.log10, 'sqrt': math.sqrt})
+        total += eval(expression, {'x': x, 'exp': math.exp, 'sin': math.sin, 'cos': math.cos, 'tan': math.tan, 'cot': lambda x: 1 / math.tan(x), 'log': math.log, 'log10': math.log10, 'sqrt': math.sqrt})
     return total * dx
 
 if __name__ == "__main__":
