792be5c94b3df913586e963e13f216a7d6efaf59
rezamarzban <73051714+rezamarzban@users.noreply.github.com>
2024-04-21 16:31:01 +0330

    Update trapz.py

--- python/trapzV2.1/trapz.py
+++ python/trapzV2.1/trapz.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     n = int(sys.argv[4])
     
     x_values = np.linspace(a, b, n)
-    y_values = eval(expression, {'x': x_values, 'exp': np.exp, 'sin': np.sin, 'cos': np.cos, 'tan': np.tan, 'cot': lambda x: 1 / np.tan(x), 'ln': np.log, 'log': np.log10, 'sqrt': np.sqrt})
+    y_values = eval(expression, {'x': x_values, 'exp': np.exp, 'sin': np.sin, 'cos': np.cos, 'tan': np.tan, 'cot': lambda x: 1 / np.tan(x), 'log': np.log, 'log10': np.log10, 'sqrt': np.sqrt})
     
     result = np.trapz(y_values, x_values)
     print("Result:", result)
